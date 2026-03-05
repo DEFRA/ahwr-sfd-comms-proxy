e10b9de9a48e7fb6d580215799d3e3469a2a93cd
rossjarvisesynergy <ross.jarvis@esynergy.co.uk>
2026-03-05 16:35:58 +0000

    feat(AHWR-1590): support endpoint to get queue messages

--- src/routes/support/support-controller.js
+++ src/routes/support/support-controller.js
@@ -4,6 +4,8 @@ import {
   getLogEntryByClaimRef,
   getLogEntryByAgreementRef
 } from '../../repositories/comms-requests-repository.js'
+import { SQSClient, ReceiveMessageCommand } from '@aws-sdk/client-sqs'
+import { config } from '../../config.js'
 
 export const getCommsRequestsHandler = async (request, h) => {
   try {
@@ -29,5 +31,45 @@ export const getCommsRequestsHandler = async (request, h) => {
     }
 
     throw Boom.internal(error)
+  }
+}
+
+export const supportQueueMessagesHandler = async (request, h) => {
+  try {
+    const { queueUrl, limit } = request.query
+
+    const client = new SQSClient({
+      region: config.get('aws.region'),
+      endpoint: config.get('aws.endpointUrl')
+    })
+
+    const command = new ReceiveMessageCommand({
+      QueueUrl: queueUrl,
+      MaxNumberOfMessages: limit,
+      VisibilityTimeout: 2,
+      WaitTimeSeconds: 0,
+      AttributeNames: ['All'],
+      MessageAttributeNames: ['All']
+    })
+    const res = await client.send(command)
+
+    request.logger.info(`Retrieved ${res.Messages?.length || 0} messages`)
+
+    const result = (res.Messages || []).map((msg) => ({
+      id: msg.MessageId,
+      body: msg.Body,
+      attributes: msg.Attributes,
+      messageAttributes: msg.MessageAttributes
+    }))
+
+    return h.response(result).code(StatusCodes.OK)
+  } catch (err) {
+    request.logger.error({ err }, 'Failed to get queue messages')
+
+    if (Boom.isBoom(err)) {
+      throw err
+    }
+
+    throw Boom.internal(err)
   }
 }
